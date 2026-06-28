7776db15bec1cb3466ab63a34eda87338952c7cd
Leandrond <113318831+Leandrond@users.noreply.github.com>
2026-06-28 20:20:52 -0300

    jugar_simon
    ponemos todo dentro de la funcion para el main

--- PlayIn EduGames/juego4_Simon.py
+++ PlayIn EduGames/juego4_Simon.py
@@ -1,31 +1,33 @@
-import subprocess   #libreria para borrar la pantalla
-import time # libreria para pausar antes de borrar (time.sleep)
-import random # libreria para tomar valores aleatorios y q cada secuencia sea diferente
-
-# 1. Funcion para limpiar la pantalla.
-def limpiar_pantalla():
-    subprocess.run("cls", shell=True)
-
-# 2. guardar el puntaje de cada juagador
-def guardar_puntaje(nombre, ronda):
-    # "a" abre el archivo para agregar texto sin borrar lo anterior
-    with open("rankingSimon.txt", "a") as archivo:
-        archivo.write(f"{nombre} - Ronda: {ronda}\n")
-    print("¡Puntaje guardado con éxito!")
-
-# 3. Función para mostrar el ranking
-def ver_ranking():
-    limpiar_pantalla()
-    print("=== TABLA DE POSICIONES ===")
-    try:
-        with open("rankingSimon.txt", "r") as archivo:
-            contenido = archivo.read()
-            if contenido == "":
-                print("Aún no se han registrado puntajes.")
-            else:
-                print(contenido)
-    except FileNotFoundError:
-        print("Aún no se han registrado puntajes.")
+def jugar_simon():
     
-    input("\nPresiona Enter para volver al menú...")
-
+    import subprocess   #libreria para borrar la pantalla
+    import time # libreria para pausar antes de borrar (time.sleep)
+    import random # libreria para tomar valores aleatorios y q cada secuencia sea diferente
+    
+    # 1. Funcion para limpiar la pantalla.
+    def limpiar_pantalla():
+        subprocess.run("cls", shell=True)
+    
+    # 2. guardar el puntaje de cada juagador
+    def guardar_puntaje(nombre, ronda):
+        # "a" abre el archivo para agregar texto sin borrar lo anterior
+        with open("rankingSimon.txt", "a") as archivo:
+            archivo.write(f"{nombre} - Ronda: {ronda}\n")
+        print("¡Puntaje guardado con éxito!")
+    
+    # 3. Función para mostrar el ranking
+    def ver_ranking():
+        limpiar_pantalla()
+        print("=== TABLA DE POSICIONES ===")
+        try:
+            with open("rankingSimon.txt", "r") as archivo:
+                contenido = archivo.read()
+                if contenido == "":
+                    print("Aún no se han registrado puntajes.")
+                else:
+                    print(contenido)
+        except FileNotFoundError:
+            print("Aún no se han registrado puntajes.")
+        
+        input("\nPresiona Enter para volver al menú...")
+    

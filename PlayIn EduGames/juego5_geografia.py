2c11a2cc2e79eb31112847ec64a5de87b1227ffc
Leandrond <113318831+Leandrond@users.noreply.github.com>
2026-06-28 19:04:24 -0300

    Inicio y diccionario del juego
    incorporacion de precentacion, librerias y diccionario para el juego

--- PlayIn EduGames/juego5_geografia.py
+++ PlayIn EduGames/juego5_geografia.py
@@ -1,3 +1,83 @@
+# Juego de Geografía Mundialista 2026
+
 def jugar_geografia():
-    print("¡Bienvenido al juego de Geografía!")
-    # Aquí iría la lógica del juego de geografía
+    
+    # while para permitir que el jugador vuelva a jugar si lo desea
+    jugar=0
+    while jugar==0:
+        
+        print("¡Bienvenido al juego de Geografía!")
+        # Inicio del juego de geografía
+        
+        
+        #importamos la librerias
+        import random # Para mezclar la lista de países y hacer el juego más dinámico
+        import unicodedata  # Para normalizar acentos y diéresis
+        
+        
+        # Definimos constantes para el nombre del archivo y el encabezado del CSV dentro de la carpeta PlayIn EduGames
+        # con el ./ para que busque en la carpeta actual y no en otra ubicación
+        NOMBRE_ARCHIVO = "./ranking_geografia.csv"
+        ENCABEZADO = "Nombre;puntos"
+        
+        # diccionario de países y sus continentes clasificados en el mundial 2026
+        paises_mundial = {
+            # --- AMÉRICA (CONMEBOL y CONCACAF) ---
+            "Canadá": "América",
+            "México": "América",
+            "Estados Unidos": "América",
+            "Argentina": "América",
+            "Brasil": "América",
+            "Colombia": "América",
+            "Ecuador": "América",
+            "Paraguay": "América",
+            "Uruguay": "América",
+            "Curazao": "América",
+            "Haití": "América",
+            "Panamá": "América",
+
+            # --- EUROPA (UEFA) ---
+            "Alemania": "Europa",
+            "Austria": "Europa",
+            "Bélgica": "Europa",
+            "Bosnia": "Europa",
+            "Croacia": "Europa",
+            "Escocia": "Europa",
+            "España": "Europa",
+            "Francia": "Europa",
+            "Inglaterra": "Europa",
+            "Noruega": "Europa",
+            "Países Bajos": "Europa",
+            "Portugal": "Europa",
+            "República Checa": "Europa",
+            "Suecia": "Europa",
+            "Suiza": "Europa",
+            "Turquía": "Europa",
+
+            # --- ASIA (AFC) ---
+            "Australia": "Asia", # Compite en la confederación asiática
+            "Irán": "Asia",
+            "Japón": "Asia",
+            "Jordania": "Asia",
+            "Corea del Sur": "Asia",
+            "Catar": "Asia",
+            "Arabia Saudí": "Asia",
+            "Uzbekistán": "Asia",
+            "Irak": "Asia",
+
+            # --- ÁFRICA (CAF) ---
+            "Argelia": "África",
+            "Cabo Verde": "África",
+            "Costa de Marfil": "África",
+            "Egipto": "África",
+            "Ghana": "África",
+            "Marruecos": "África",
+            "Senegal": "África",
+            "Sudáfrica": "África",
+            "Túnez": "África",
+            "Congo": "África",
+
+            # --- OCEANÍA (OFC) ---
+            "Nueva Zelanda": "Oceanía"
+        }
+        
